1c3271278c427654b049bc1471dbe62a3a2642fd
donglai96 <donglaima96@gmail.com>
2019-11-30 23:38:28 -0800

    LRP

---
+++ parameter.py
@@ -0,0 +1,14 @@
+'''
+This File is the parameter of wave growth fild
+'''
+import numpy as np
+import mpmath as mp
+import scipy.special
+
+emass = 9.10938291e-31
+pmass = 1.67262178e-27
+echarge = 1.60217657e-19
+permittivity = 8.854187817e-12
+permeability = 4 * np.pi * 1e-7
+cspeed = 299792458
+boltzmann = 1.3806488e-23
